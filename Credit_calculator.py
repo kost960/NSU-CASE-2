02fc035e50eb0ebc734fa6d2a99e0efc8205e727
kost960 <pozitq3@gmail.com>
2024-02-25 22:23:03 +0700

    Create class & create language logic

--- Credit_calculator.py
+++ Credit_calculator.py
@@ -1,3 +1,7 @@
+import ru_local as ru
+import en_local as en
+
+
 class BankInterest():
     def __init__(self, summ, percent, period):
         self.summ = summ
@@ -5,13 +9,41 @@ class BankInterest():
         self.percent = percent
 
     def diff_int(self):
-        arr = []
-        mp_cnt = self.period * 12
+        payments = []
+        months = self.period * 12
         rest = self.summ
-        mp_real = self.summ / (self.period * 12.0)
-        while mp_cnt != 0:
-            mp = mp_real + (rest * self.percent / 1200)
-            arr.append(round(mp, 2))
+        mp_real = self.summ / months
+        while months != 0:
+            month_paid = mp_real + (rest * self.percent / 1200)
+            payments.append(round(month_paid, 2))
             rest = rest - mp_real
-            mp_cnt = mp_cnt - 1
-        return arr
+            months = months - 1
+        return payments
+
+
+if __name__ == '__main__':
+    lang = input('Choose language(ru/en): ')
+    if lang != 'ru' and lang != 'en':
+        print("Wrong language!!!")
+
+    if lang == 'ru':
+        summ = int(input(f'{ru.SUMM_INPUT}: '))
+        percent = int(input(f'{ru.PERCENT_INPUT}: '))
+        period = int(input(f'{ru.PERIOD_INPUT}: '))
+
+        credit = BankInterest(summ, percent, period)
+        diff = credit.diff_int()
+        for month, pay in enumerate(diff):
+            print(f'{ru.PAYMENT_PRINT} {month + 1}: {pay: .2f}.')
+        print(round(sum(diff), 2))
+
+    if lang == 'en':
+        summ = int(input(f'{en.SUMM_INPUT}: '))
+        percent = int(input(f'{en.PERCENT_INPUT}: '))
+        period = int(input(f'{en.PERIOD_INPUT}: '))
+
+        credit = BankInterest(summ, percent, period)
+        diff = credit.diff_int()
+        for month, pay in enumerate(diff):
+            print(f'{ru.PAYMENT_PRINT} {month + 1}: {pay: .2f}.')
+        print(round(sum(diff), 2))
